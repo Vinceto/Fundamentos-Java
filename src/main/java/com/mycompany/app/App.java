9d40a76fd37012ce12ea78b4662e6e75acbd4649
Andres Sepulveda <86821638+Andressep@users.noreply.github.com>
2024-05-06 23:09:15 -0400

    Initial commit

---
+++ src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,15 @@
+/*----------------------------------------------------------------------------------------
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See LICENSE in the project root for license information.
+ *---------------------------------------------------------------------------------------*/
+
+package com.mycompany.app;
+
+public class App {
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("Agregando el commit inicial.");
+    }
+}
